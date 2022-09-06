62dbc959a51b65d74d37cc2860047d7ca8f1c135
Milena Damnjanovic <mimi.damnjanovic16@gmail.com>
2022-09-06 18:20:09 -0400

    Fix add plant button onClick issue

--- frontend/src/components/Plants/AddButton.jsx
+++ frontend/src/components/Plants/AddButton.jsx
@@ -1,11 +1,9 @@
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import IconButton from '@mui/material/IconButton';
 
 const AddButton= ({onClick}) => {
   return (
-    <IconButton onClick={onClick}>
-      <AddCircleOutlineIcon className="plus-icon icon2" />
+    <IconButton className="plus-button" onClick={onClick}>
       <AddCircleIcon className="plus-icon" />
     </IconButton>
   );
