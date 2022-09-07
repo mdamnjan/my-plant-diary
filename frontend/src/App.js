a03622ad5eb20b17ad7a8bd556bad3435dfaae99
Milena Damnjanovic <mimi.damnjanovic16@gmail.com>
2022-09-07 14:29:54 -0400

    Use routing for PlantPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,17 @@ import "./App.css";
 import NotePage from "./components/Notes/NotePage";
 import LoginPage from "./components/Login/LoginPage";
 import PlantPage from "./components/Plants/PlantPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <PlantPage />
+        <Router>
+          <Routes>
+            <Route path="/" element={<PlantPage />} />
+          </Routes>
+        </Router>
       </header>
     </div>
   );
