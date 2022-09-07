5b573e3ef025f4713ee942aa183383cb094f9a21
Milena Damnjanovic <mimi.damnjanovic16@gmail.com>
2022-09-07 15:14:24 -0400

    Add ability to delete plant from detail page

--- frontend/src/components/Plants/PlantDetailPage.jsx
+++ frontend/src/components/Plants/PlantDetailPage.jsx
@@ -1,17 +1,29 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import PlantCard from "./PlantCard";
+import axios from "axios";
+
+
+let tempAuth = { auth: { username: "admin", password: "admin" } };
 
 const PlantDetailPage = () => {
   const location = useLocation();
+  const navigate = useNavigate()
   const plant = location.state;
+  const handleDelete = (plant) => {
+    axios.delete(`/plants/${plant.id}/`, tempAuth).then(()=>navigate("/"));
+  };
+
   return (
     <div className="plant-detail-container">
-      <h1>{plant.name}</h1>
+        <PlantCard plant={plant} handleDelete={handleDelete}/>
+
+      {/* <h1>{plant.name}</h1>
       <img
         className="plant-profile-img"
         src="../../Calathea_orbifolia.jpg"
       ></img>
       <h3>Watering Frequency</h3>
-      <p>{plant.watering_frequency}</p>
+      <p>{plant.watering_frequency}</p> */}
     </div>
   );
 };
