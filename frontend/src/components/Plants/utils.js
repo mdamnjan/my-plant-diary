9b1fd7c860d913942fc8ee93efbb19f0a826b509
Milena Damnjanovic <mimi.damnjanovic16@gmail.com>
2022-09-09 19:44:49 -0400

    Add utils for watering entry CRUD

--- frontend/src/components/Plants/utils.js
+++ frontend/src/components/Plants/utils.js
@@ -15,10 +15,22 @@ export const createPlant = (body) => {
   return axios.post("/plants/", body, auth);
 };
 
+export const createWateringEntry = (body) => {
+    return axios.post("/watering/", body, auth);
+  };
+
 export const updatePlant = (plantID, body) => {
   return axios.put(`/plants/${plantID}/`, body, auth);
 };
 
+export const updateWateringEntry = (entryID, body) => {
+    return axios.put(`/watering/${entryID}/`, body, auth);
+  };
+
 export const deletePlant = (plantID) => {
   return axios.delete(`/plants/${plantID}/`, auth);
 };
+
+export const deleteWateringEntry = (entryID) => {
+    return axios.delete(`/watering/${plantID}/`, auth);
+  };
