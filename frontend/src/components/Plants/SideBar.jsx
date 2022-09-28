0d3bebf10b315ea4a98f7a39aa48d03d8afc1092
Milena Damnjanovic <mimi.damnjanovic16@gmail.com>
2022-09-27 23:20:57 -0400

    Add svg icon as fake logo

--- frontend/src/components/Plants/SideBar.jsx
+++ frontend/src/components/Plants/SideBar.jsx
@@ -1,24 +1,28 @@
 import HistoryWidget from "./HistoryWidget";
 
-import { Paper, Button } from "@mui/material";
-import { Home, Yard, LocalDrink, AccountCircle } from "@mui/icons-material";
+import { Paper, Button, Typography } from "@mui/material";
+import { Home, LocalFlorist, LocalDrink, AccountCircle } from "@mui/icons-material";
+
 const SideBar = ({ navigation, onClick }) => {
   const buttons = [
-    { text: "Home", icon: <Home /> },
-    { text: "My Plants", icon: <Yard /> },
-    { text: "Watering Entries", icon: <LocalDrink /> },
-    { text: "Profile", icon: <AccountCircle /> },
+    { text: "Home", icon: <Home fontSize="large"/> },
+    { text: "My Plants", icon: <LocalFlorist fontSize="large"/> },
+    { text: "Watering", icon: <LocalDrink fontSize="large"/> },
+    { text: "Profile", icon: <AccountCircle fontSize="large"/> },
   ];
   if (navigation) {
     return (
       <Paper className="side-bar">
+        <Typography style={{display: "flex"}} >
+        <img style={{width: "30px", margin: "10px"}} src="leaf-svgrepo-com.svg"/>
+        <h2>My Plant Diary</h2></Typography>
         {buttons.map((button) => (
           <Button style={{display: "flex"}}>
             {button.icon}
-            <h2>{button.text}</h2>
+            <h2 style={{marginLeft: "5px"}} >{button.text}</h2>
           </Button>
         ))}
-        <Button onClick={onClick} style={{width: "100%"}} variant="contained">Add a Plant</Button>
+        <Button onClick={onClick} style={{width: "100%", marginTop: "5%"}} variant="contained">Add a Plant</Button>
       </Paper>
     );
   } else {
