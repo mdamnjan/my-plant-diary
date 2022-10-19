bb7f7acd97398f15eda1d0d5a811957ec6f66d55
mdamnjan <mimi.damnjanovic16@gmail.com>
2022-10-19 18:18:11 -0400

    Restyle plant detail page

--- frontend/src/components/Plants/PlantDetailPage.jsx
+++ frontend/src/components/Plants/PlantDetailPage.jsx
@@ -3,8 +3,10 @@ import HistoryWidget from "./HistoryWidget";
 import PlantCardV2 from "./PlantCard";
 import { createWateringEntry, deletePlant, fetchPlants, fetchWateringEntries } from "./utils";
 import AddButton from "../common/AddButton";
+import { Typography } from "@mui/material";
 import { useState } from "react";
 import WateringEntryForm from "./WateringEntryForm";
+import { useEffect } from "react";
 
 const PlantDetailPage = () => {
   const location = useLocation();
@@ -18,8 +20,12 @@ const PlantDetailPage = () => {
 
   return (
     <div className="plant-detail-container">
-      <PlantCardV2 plant={plant} handleDelete={handleDelete} />
-      <HistoryWidget />
+      <img src="../../Calathea_orbifolia.jpg"></img>
+      <Typography variant="h5">{plant.name}</Typography>
+      <Typography variant="body1">Last Watered: {plant.last_watered}</Typography>
+      <Typography variant="body1">Next Watering: {plant.next_watering}</Typography>
+      <Typography variant="body1">Watering Frequency: {plant.watering_frequency}</Typography>
+      <div></div>
       <WateringEntryForm
         open={open}
         onClose={() => setOpen(false)}
