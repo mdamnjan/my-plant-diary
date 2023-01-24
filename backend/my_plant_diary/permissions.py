d3b1ae77d9d72a0e9791588238cfffba203c5239
mdamnjan <mimi.damnjanovic16@gmail.com>
2023-01-23 21:01:23 -0500

    Restrict API read access to owners only

--- backend/my_plant_diary/permissions.py
+++ backend/my_plant_diary/permissions.py
@@ -1,8 +1,8 @@
 from rest_framework import permissions
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsOwner(permissions.BasePermission):
     """
-    Custom permission to only allow owners of an object to edit it.
+    Custom permission to only allow owners of an object to edit it and see it.
     """
 
     def has_object_permission(self, request, view, obj):
