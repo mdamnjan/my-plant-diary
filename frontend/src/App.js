3aa398d9540e87693fece7cd2a6eb0f71d9d0681
mdamnjan <mimi.damnjanovic16@gmail.com>
2023-09-26 20:04:52 -0400

    Add basic homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,23 @@ import logo from "./logo.svg";
 import "./App.css";
 import NotePage from "./components/Notes/NotePage";
 import LoginPage from "./components/Login/LoginPage";
-import PlantPage from "./components/Plants/PlantPage"
+import PlantPage from "./components/Plants/PlantPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PlantDetailPage from "./components/Plants/PlantDetailPage";
+import HomePage from "./components/Home/HomePage";
 
 function App() {
   return (
     <div className="App">
-        <header className="App-header"></header>
-        <Router>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/plants" element={<PlantPage />} />
-            <Route path="/plants/:slug/" element={<PlantDetailPage />} />
-          </Routes>
-        </Router>
+      <header className="App-header"></header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/plants" element={<PlantPage />} />
+          <Route path="/plants/:slug/" element={<PlantDetailPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
