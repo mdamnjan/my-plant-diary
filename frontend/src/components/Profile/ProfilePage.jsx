d6a2a1c6d19246b894ebfdc41754e647ec96e796
mdamnjan <mimi.damnjanovic16@gmail.com>
2023-11-14 19:39:39 -0500

    Delete unused parts of profile page

--- frontend/src/components/Profile/ProfilePage.jsx
+++ frontend/src/components/Profile/ProfilePage.jsx
@@ -4,13 +4,20 @@ import {
   Avatar,
   Divider,
   TextField,
-  FormGroup,
-  FormControl,
-  FormControlLabel,
-  Switch,
 } from "@mui/material";
 
+import { useQuery } from "react-query";
+import { fetchUser } from "../../api";
+
 const ProfilePage = () => {
+  const { data: user } = useQuery({
+    queryKey: ["user"],
+    queryFn: () => fetchUser(),
+    initialData: {
+      username: "",
+      email: ""
+    },
+  });
   return (
     <div>
       <Box sx={{ width: "100%" }}>
@@ -18,7 +25,7 @@ const ProfilePage = () => {
       </Box>
       <Box sx={{ padding: "20px", display: "flex", alignItems: "center" }}>
         <Avatar sx={{ width: 100, height: 100, marginRight: "20px" }} />
-        <Typography variant="h4">mdamnjan</Typography>
+        <Typography variant="h4">{user.username}</Typography>
       </Box>
       <Box sx={{ padding: "20px" }}>
         <Typography variant="h4">Details</Typography>
@@ -33,66 +40,16 @@ const ProfilePage = () => {
         >
           <div>
             <Typography variant="h5">email</Typography>{" "}
-            <TextField disabled value="mdamnjan@gmail.com" />
+            <TextField disabled value={user.email} />
           </div>
           <div>
             <Typography variant="h5">username</Typography>{" "}
-            <TextField disabled value="mdamnjan" />
+            <TextField disabled value={user.username} />
           </div>
           <div>
-            {" "}
             <Typography variant="h5">password</Typography>{" "}
             <TextField disabled type="password" value="password" />
           </div>
-        </div>
-        <Typography variant="h4">Settings</Typography>
-        <Divider />
-        <div style={{ marginTop: "20px" }}>
-          <Typography
-            variant="h5"
-            style={{ marginRight: "20px", display: "block" }}
-          >
-            Mode
-          </Typography>{" "}
-          <span>Light</span>
-          <Switch checked></Switch>
-          <span>Dark</span>
-          <Typography
-            variant="h5"
-            style={{ marginRight: "20px", display: "block" }}
-          >
-            Alerts
-          </Typography>{" "}
-          <Typography
-            variant="body"
-            style={{ marginRight: "20px", display: "block" }}
-          >
-            Get alerts when this type of task is overdue:
-          </Typography>{" "}
-          <FormControl
-            style={{ marginLeft: "50px" }}
-            component="fieldset"
-            variant="standard"
-          >
-            <FormGroup>
-              <FormControlLabel
-                control={<Switch checked name="water" />}
-                label="Watering"
-              />
-              <FormControlLabel
-                control={<Switch checked name="water" />}
-                label="Repotting"
-              />
-              <FormControlLabel
-                control={<Switch checked name="water" />}
-                label="Progress Update"
-              />
-              <FormControlLabel
-                control={<Switch checked name="water" />}
-                label="Propagate"
-              />
-            </FormGroup>
-          </FormControl>
         </div>
       </Box>
     </div>
